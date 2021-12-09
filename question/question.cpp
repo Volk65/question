5408c7c55025bd17c88930c9939cd573cad40f46
user5-7 <user5-7@citp.energypk.ru>
2021-12-09 13:26:58 +0300

    Добавьте файлы проекта.

---
+++ question/question.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	string fname, lname, location;
+	int age;
+	cout << "First name: ";
+	cin >> fname;
+	cout << "Last name: ";
+	cin >> lname;
+	cout << "age: ";
+	cin >> age;
+	cout << "Location: ";
+	cin >> location;
+	cout << endl << "First Name: " << fname << endl << "Last Name: " << lname << endl << "Age: " << age << endl << "Location: " << location;
+
+}
